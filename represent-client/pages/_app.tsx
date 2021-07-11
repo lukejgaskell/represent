be1aca5f9a1d4e7741831dcbf2330f72b834bdbd
Luke Gaskell <lukejgaskell@gmail.com>
2021-07-11 18:43:50 -0400

    updated client

--- represent-client/pages/_app.tsx
+++ represent-client/pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
+import Meta from '@/components/meta'
+import '@/styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+	return (
+		<ThemeProvider
+			attribute='class'
+			defaultTheme='system'
+			disableTransitionOnChange
+		>
+			<Meta />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	)
 }
-export default MyApp
+
+export default App
