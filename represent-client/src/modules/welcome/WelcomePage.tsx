69a9e489e87e3bcf8f9b2bea52d05ff5da4b682b
Luke Gaskell <lukejgaskell@gmail.com>
2021-07-27 23:50:21 -0400

    async save settings

--- represent-client/src/modules/welcome/WelcomePage.tsx
+++ represent-client/src/modules/welcome/WelcomePage.tsx
@@ -1,38 +1,52 @@
-import { Button } from "@material-ui/core"
-import { FocusedLayout } from "@/components/layouts/FocusedLayout"
-import React from "react"
-import { useRouter } from "next/router"
-import { useUserStore } from "@/stores/user/useUserStore"
+import { Button } from '@material-ui/core'
+import { FocusedLayout } from '@/components/layouts/FocusedLayout'
+import React from 'react'
+import { useRouter } from 'next/router'
+import { useUserStore } from '@/stores/user/useUserStore'
 
 export const WelcomePage = () => {
-  const { saveSettings } = useUserStore()
-  const { replace } = useRouter()
+	const { saveSettings } = useUserStore()
+	const { replace } = useRouter()
 
-  function handleContinue() {
-    saveSettings({ hasSeenWelcome: true })
-    replace("/")
-  }
-  return (
-    <FocusedLayout title="Welcome">
-      <section className="pr-4 pl-4 mt-5">
-        <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Welcome to Represent</h2>
+	async function handleContinue() {
+		await saveSettings({ hasSeenWelcome: true })
+		replace('/')
+	}
+	return (
+		<FocusedLayout title='Welcome'>
+			<section className='pr-4 pl-4 mt-5'>
+				<h2 className='text-xl font-semibold text-gray-800 dark:text-gray-200'>
+					Welcome to Represent
+				</h2>
 
-        <p className="mt-2 text-gray-600 dark:text-gray-400">
-          We aim to give you upfront information about what your elected officials are doing on your behalf. Represent presents information about what&apos;s happening in the house
-          and senate.
-        </p>
+				<p className='mt-2 text-gray-600 dark:text-gray-400'>
+					We aim to give you upfront information about what your elected
+					officials are doing on your behalf. Represent presents information
+					about what&apos;s happening in the house and senate.
+				</p>
 
-        <h2 className="text-l mt-6 font-semibold text-gray-800 dark:text-gray-200">Getting Started</h2>
+				<h2 className='text-l mt-6 font-semibold text-gray-800 dark:text-gray-200'>
+					Getting Started
+				</h2>
 
-        <p className="mt-2 text-gray-600 dark:text-gray-400">1. You can keep up with what votes have recently been voted on in Congress by clicking on the votes tab.</p>
-        <p className="mt-2 text-gray-600 dark:text-gray-400 mb-20">
-          2. Take a look at the representatives tab where you can see who your reprenatives are and how they&apos;ve been voting. You can also find useful links to get more
-          information about what they are doing.
-        </p>
-        <Button variant="contained" color="primary" fullWidth onClick={handleContinue}>
-          Continue
-        </Button>
-      </section>
-    </FocusedLayout>
-  )
+				<p className='mt-2 text-gray-600 dark:text-gray-400'>
+					1. You can keep up with what votes have recently been voted on in
+					Congress by clicking on the votes tab.
+				</p>
+				<p className='mt-2 text-gray-600 dark:text-gray-400 mb-20'>
+					2. Take a look at the representatives tab where you can see who your
+					reprenatives are and how they&apos;ve been voting. You can also find
+					useful links to get more information about what they are doing.
+				</p>
+				<Button
+					variant='contained'
+					color='primary'
+					fullWidth
+					onClick={handleContinue}
+				>
+					Continue
+				</Button>
+			</section>
+		</FocusedLayout>
+	)
 }
--- represent-client/src/stores/user/useUserStore.ts
+++ represent-client/src/stores/user/useUserStore.ts
@@ -1,46 +1,48 @@
-import { getUserSettings, saveUserData } from "@/queries/user"
+import { getUserSettings, saveUserData } from '@/queries/user'
 
-import { UserData } from "@/stores/user/UserData.type"
-import create from "zustand"
-import { persist } from "zustand/middleware"
-import { useErrorStore } from "../useErrorStore"
+import { UserData } from '@/stores/user/UserData.type'
+import create from 'zustand'
+import { persist } from 'zustand/middleware'
+import { useErrorStore } from '../useErrorStore'
+
+let saveTimeout: ReturnType<typeof setTimeout>
 
 type SettingsStore = {
-  settings: UserData
-  isLoading: boolean
-  saveSettings: (data: UserData) => Promise<void>
-  loadSettings: () => Promise<void>
+	settings: UserData
+	isLoading: boolean
+	saveSettings: (data: UserData) => Promise<void>
+	loadSettings: () => Promise<void>
 }
 
 export const useUserStore = create<SettingsStore>(
-  persist(
-    (set, get) => ({
-      settings: {} as UserData,
-      isLoading: false,
-      saveSettings: async (userData: UserData) => {
-        const updatedSettings = { ...get().settings, ...userData }
-        const { data, error } = await saveUserData(updatedSettings)
-        const { addError } = useErrorStore.getState()
+	persist(
+		(set, get) => ({
+			settings: {} as UserData,
+			isLoading: false,
+			saveSettings: async (userData: UserData) => {
+				const updatedSettings = { ...get().settings, ...userData }
+				set({ settings: updatedSettings })
 
-        if (error) {
-          return addError(error.message)
-        }
-        set({ settings: updatedSettings })
-      },
-      loadSettings: async () => {
-        const { addError } = useErrorStore.getState()
-        set({ isLoading: true })
-        const { data, error } = await getUserSettings()
-        if (error) {
-          set({ isLoading: false })
-          addError(error.message)
-        }
+				if (saveTimeout) clearTimeout(saveTimeout)
 
-        set({ settings: data || ({} as UserData), isLoading: false })
-      },
-    }),
-    {
-      name: "settings-store",
-    }
-  )
+				saveTimeout = setTimeout(async () => {
+					await saveUserData(updatedSettings)
+				}, 2000)
+			},
+			loadSettings: async () => {
+				const { addError } = useErrorStore.getState()
+				set({ isLoading: true })
+				const { data, error } = await getUserSettings()
+				if (error) {
+					set({ isLoading: false })
+					addError(error.message)
+				}
+
+				set({ settings: data || ({} as UserData), isLoading: false })
+			},
+		}),
+		{
+			name: 'settings-store',
+		}
+	)
 )
