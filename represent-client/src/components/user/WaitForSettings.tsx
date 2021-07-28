4cc26eae6337172f406e3771e1f4b0839cdac567
Luke Gaskell <lukejgaskell@gmail.com>
2021-07-27 23:43:29 -0400

    remove screen flash

--- represent-client/src/components/user/WaitForSettings.tsx
+++ represent-client/src/components/user/WaitForSettings.tsx
@@ -1,36 +1,34 @@
-import React, { useEffect, useState } from "react"
-
-import { UserData } from "@/stores/user/UserData.type"
-import { getUserSettings } from "@/queries/user"
-import { useErrorStore } from "@/stores/useErrorStore"
-import { useRouter } from "next/router"
-import { useUserStore } from "@/stores/user/useUserStore"
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useUserStore } from '@/stores/user/useUserStore'
 
 type WaitForSettings = {}
 
 export const WaitForSettings: React.FC<WaitForSettings> = ({ children }) => {
-  const { settings, loadSettings, isLoading } = useUserStore()
-  const hasSettings = !!settings.district && !!settings.state
-  const hasSeenWelcome = settings.hasSeenWelcome
-  const { replace } = useRouter()
+	const [isNotYetLoading, setIsNotYetLoading] = useState(true)
+	const { settings, loadSettings, isLoading } = useUserStore()
+	const hasSettings = !!settings.district && !!settings.state
+	const hasSeenWelcome = settings.hasSeenWelcome
+	const { replace } = useRouter()
 
-  useEffect(() => {
-    loadSettings()
-  }, [])
+	useEffect(() => {
+		loadSettings()
+		setIsNotYetLoading(false)
+	}, [])
 
-  if (isLoading && (!hasSettings || !hasSeenWelcome)) {
-    return null
-  }
+	if ((isLoading || isNotYetLoading) && (!hasSettings || !hasSeenWelcome)) {
+		return null
+	}
 
-  if (!hasSettings) {
-    replace(`/intro`)
-    return null
-  }
+	if (!hasSettings) {
+		replace(`/intro`)
+		return null
+	}
 
-  if (!hasSeenWelcome) {
-    replace(`/welcome`)
-    return null
-  }
+	if (!hasSeenWelcome) {
+		replace(`/welcome`)
+		return null
+	}
 
-  return <>{children}</>
+	return <>{children}</>
 }
