a9e4b7e9dcbaedb4cd93e3edfaedae02e9878d87
Luke Gaskell <lukejgaskell@gmail.com>
2021-07-29 00:39:28 -0400

    added back overfitting header

--- represent-client/src/components/meta.tsx
+++ represent-client/src/components/meta.tsx
@@ -17,7 +17,10 @@ const Meta = () => (
 			content='Keep up to date with what your representatives are doing on your behalf'
 		/>
 		<meta name='theme-color' content='#1d2020' />
-		<meta name='viewport' content='width=device-width, initial-scale=1' />
+		<meta
+			name='viewport'
+			content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
+		/>
 		<link rel='apple-touch-icon' href='/images/apple-touch-icon.png' />
 		<link rel='icon' type='image/png' href='/images/favicon.png' />
 		<link rel='manifest' href='/manifest.json' />
