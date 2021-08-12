db7a8d0033a3b0b5e8ebdaef8a12cf27d169fbdd
Luke Gaskell <lukejgaskell@gmail.com>
2021-08-12 00:12:17 -0400

    updated statements sync

--- backend/statements-sync.handler.js
+++ backend/statements-sync.handler.js
@@ -4,25 +4,23 @@ const { createClient } = require("@supabase/supabase-js")
 
 const supabase = createClient("https://ijxfwjuurxppacepegmf.supabase.in", process.env.SUPABASE_SERVICE_KEY)
 
-const statementsUrl = `https://api.propublica.org/congress/v1/statements/latest.json`
+const getStatementsUrl = today => `https://api.propublica.org/congress/v1/statements/${today}.json`
 
 const API_KEY = process.env.API_KEY
 
 module.exports.run = async (event, context) => {
   console.info(`Cron function "${context.functionName}" is starting`)
 
+  const today = new Date().toISOString().split("T")[0]
+
   try {
-    const statementsResponse = await axios.get(statementsUrl, { headers: { "X-API-Key": API_KEY } }).then(r => r.data)
-    const today = new Date()
-    const tomorrow = today.setDate(today.getDate() + 1)
-    const statements = statementsResponse.results
-      .filter(s => new Date(s.date) < tomorrow)
-      .map(s => ({
-        id: `${s.member_id}-${s.date}`,
-        member_id: s.member_id,
-        date: s.date,
-        metadata: { ...s },
-      }))
+    const statementsResponse = await axios.get(getStatementsUrl(today), { headers: { "X-API-Key": API_KEY } }).then(r => r.data)
+    const statements = statementsResponse.results.map(s => ({
+      id: `${s.member_id}-${s.date}`,
+      member_id: s.member_id,
+      date: s.date,
+      metadata: { ...s },
+    }))
 
     const { data, error } = await supabase.from("statements").upsert(statements, { returning: "minimal" })
 
