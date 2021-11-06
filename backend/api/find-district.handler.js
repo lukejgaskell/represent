74d0be620a47437060721de02f91f4dff7565bfb
Luke Gaskell <lukejgaskell@gmail.com>
2021-11-06 19:16:33 -0400

    updated backend to have api

---
+++ backend/api/find-district.handler.js
@@ -0,0 +1,15 @@
+"use strict"
+const Geocodio = require("geocodio-library-node")
+
+const geocoder = new Geocodio(process.env.GEOCODIO_TOKEN)
+
+module.exports.run = async (event, context) => {
+  console.info(`Api function "${context.functionName}" has been called`)
+  const { address } = event
+  const result = await geocoder.geocode(address, ["cd"]).then(r => r.results[0])
+
+  const district = result.fields.congressional_districts[0].district_number
+  const state = result.address_components.state
+
+  return { ...result, district, state, fields: undefined }
+}
