115fc7792c89f617ded182fe6d699836d55ce912
Luke Gaskell <lukejgaskell@gmail.com>
2021-11-06 19:52:12 -0400

    logging

--- backend/api/find-district.handler.js
+++ backend/api/find-district.handler.js
@@ -5,6 +5,7 @@ const geocoder = new Geocodio(process.env.GEOCODIO_TOKEN)
 
 module.exports.run = async (event, context) => {
   console.info(`Api function "${context.functionName}" has been called`)
+  console.info("event", event)
   const { address } = event
   const result = await geocoder.geocode(address, ["cd"]).then(r => r.results[0])
 
