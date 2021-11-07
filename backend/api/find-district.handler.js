76f30f62bab56bc5ad187b025a7ddaf04748dad5
Luke Gaskell <lukejgaskell@gmail.com>
2021-11-06 20:16:46 -0400

    try catch

--- backend/api/find-district.handler.js
+++ backend/api/find-district.handler.js
@@ -5,12 +5,15 @@ const geocoder = new Geocodio(process.env.GEOCODIO_TOKEN)
 
 module.exports.run = async (event, context) => {
   console.info(`Api function "${context.functionName}" has been called`)
-  console.info("event", event)
   const { address } = event.queryStringParameters
-  const result = await geocoder.geocode(address, ["cd"]).then(r => r.results[0])
+  try {
+    const result = await geocoder.geocode(address, ["cd"]).then(r => r.results[0])
 
-  const district = result.fields.congressional_districts[0].district_number
-  const state = result.address_components.state
-
-  return { ...result, district, state, fields: undefined }
+    console.info(result)
+    const district = result.fields.congressional_districts[0].district_number
+    const state = result.address_components.state
+    return { ...result, district, state, fields: undefined }
+  } catch (e) {
+    return e
+  }
 }
