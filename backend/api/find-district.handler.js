00acd8825bbe3f760698ff2bfdca27335be3f011
Luke Gaskell <lukejgaskell@gmail.com>
2021-11-06 20:04:16 -0400

    fixed path

--- backend/api/find-district.handler.js
+++ backend/api/find-district.handler.js
@@ -6,7 +6,7 @@ const geocoder = new Geocodio(process.env.GEOCODIO_TOKEN)
 module.exports.run = async (event, context) => {
   console.info(`Api function "${context.functionName}" has been called`)
   console.info("event", event)
-  const { address } = event
+  const { address } = event.queryStringParameters
   const result = await geocoder.geocode(address, ["cd"]).then(r => r.results[0])
 
   const district = result.fields.congressional_districts[0].district_number
