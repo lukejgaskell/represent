9d0219118a8c6f3f3bc72c75885dbb289d672ea2
Luke Gaskell <ljgaskell@liberty.edu>
2022-02-24 00:47:22 -0800

    hopefully fixed color scheme issues

--- mobile/hooks/useColorScheme.ts
+++ mobile/hooks/useColorScheme.ts
@@ -1,19 +1,32 @@
 import { Appearance, ColorSchemeName } from "react-native"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
-export default function useColorScheme(): NonNullable<ColorSchemeName> {
+export default function useColorScheme(delay = 500): NonNullable<ColorSchemeName> {
   const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme())
+
+  let timeout = useRef<NodeJS.Timeout | null>(null).current
 
   useEffect(() => {
     Appearance.addChangeListener(onColorSchemeChange)
 
     return () => {
+      resetCurrentTimeout()
       Appearance.removeChangeListener(onColorSchemeChange)
     }
   }, [])
 
-  function onColorSchemeChange() {
-    setColorScheme(Appearance.getColorScheme())
+  function onColorSchemeChange(preferences: Appearance.AppearancePreferences) {
+    resetCurrentTimeout()
+
+    timeout = setTimeout(() => {
+      setColorScheme(Appearance.getColorScheme())
+    }, delay)
+  }
+
+  function resetCurrentTimeout() {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
   }
 
   return colorScheme as NonNullable<ColorSchemeName>
