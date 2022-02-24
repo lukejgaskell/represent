81e3219438522bb7b4fc3ae5c83ae42531a13e93
Luke Gaskell <ljgaskell@liberty.edu>
2022-02-23 23:15:44 -0800

    added apple auth

--- mobile/screens/AuthScreen.tsx
+++ mobile/screens/AuthScreen.tsx
@@ -1,15 +1,15 @@
+import * as AppleAuthentication from "expo-apple-authentication"
 import * as AuthSession from "expo-auth-session"
 
 import { Button, Text } from "react-native-paper"
-import Colors, { IColors } from "../constants/Colors"
 import React, { useState } from "react"
 import { StyleSheet, View } from "react-native"
 import supabaseClient, { supabaseAuthUrl } from "../lib/supabaseClient"
 
+import Colors from "../constants/Colors"
 import Constants from "expo-constants"
 import DarkLogo from "../components/images/DarkLogo"
 import GoogleIcon from "../components/images/GoogleIcon"
-import LightLogo from "../components/images/LightLogo"
 import useColorScheme from "../hooks/useColorScheme"
 
 export default function AuthScreen() {
@@ -17,9 +17,8 @@ export default function AuthScreen() {
   const colorScheme = useColorScheme()
   const styles = createStyles(colorScheme)
 
-  async function loginWithGoogle() {
+  async function login(provider: string) {
     setErrorMessage(null)
-    const provider = "google"
 
     const SCHEME = Constants.manifest?.scheme
     const proxyRedirectUri = AuthSession.makeRedirectUri({
@@ -52,9 +51,16 @@ export default function AuthScreen() {
   return (
     <View style={styles.container}>
       <DarkLogo />
-      <Button uppercase={false} style={styles.button} mode="contained" onPress={loginWithGoogle} icon={GoogleIcon}>
-        <Text style={styles.buttonText}>Continue With Google</Text>
+      <Button uppercase={false} style={styles.googleButton} mode="contained" onPress={() => login("google")} icon={GoogleIcon}>
+        <Text style={styles.googleButtonText}>Continue With Google</Text>
       </Button>
+      <AppleAuthentication.AppleAuthenticationButton
+        buttonType={AppleAuthentication.AppleAuthenticationButtonType.CONTINUE}
+        buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.WHITE}
+        cornerRadius={5}
+        style={styles.appleButton}
+        onPress={() => login("apple")}
+      />
       <Text>{errorMessage}</Text>
     </View>
   )
@@ -68,16 +74,26 @@ const createStyles = (colorScheme: "light" | "dark") =>
       alignItems: "center",
       justifyContent: "center",
     },
-    button: {
+    googleButton: {
       marginTop: 100,
       backgroundColor: Colors.dark.text,
-      borderRadius: 10,
+      borderRadius: 5,
       paddingRight: 10,
       paddingLeft: 10,
+      marginBottom: 40,
+      width: 290,
+      height: 44,
     },
-    buttonText: {
-      fontSize: 18,
+    appleButton: {
+      width: 290,
+      height: 44,
+      marginBottom: 20,
+    },
+    googleButtonText: {
+      fontSize: 15,
+      lineHeight: 24,
       color: Colors.light.text,
+      fontWeight: "600",
     },
     separator: {
       marginVertical: 30,
