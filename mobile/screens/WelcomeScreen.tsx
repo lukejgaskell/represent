1102fc4e1306165d63b5e7a456579bd090eacf83
Luke Gaskell <ljgaskell@liberty.edu>
2022-04-05 21:43:17 -0400

    fixed typos

--- mobile/screens/WelcomeScreen.tsx
+++ mobile/screens/WelcomeScreen.tsx
@@ -22,15 +22,15 @@ export default function WelcomeScreen() {
         <Title style={styles.titleOne}>Welcome to Represent</Title>
 
         <Paragraph>
-          We aim to give you simple information about what your elected officials are doing on your behalf. Represent presents information about what&apos;s happening in the house
-          and senate.
+          We aim to give you simple information about what your elected officials are doing on your behalf. Represent presents information about what&apos;s happening in the House
+          and Senate.
         </Paragraph>
 
         <Title style={styles.titleTwo}>Getting Started</Title>
 
         <Paragraph>1. You can keep up with what votes have recently been voted on in Congress by clicking on the votes tab.</Paragraph>
         <Paragraph>
-          2. Take a look at the representatives tab where you can see who your reprenatives are and how they&apos;ve been voting. You can also find useful links to get more
+          2. Take a look at the representatives tab where you can see who your representatives are and how they&apos;ve been voting. You can also find useful links to get more
           information about what they are doing.
         </Paragraph>
         <Button style={styles.button} mode="contained" onPress={handleContinue}>
