2d6e0b0dc29e81a74faa0043427acbbd439b1845
Luke Gaskell <lukejgaskell@gmail.com>
2022-07-28 15:59:56 -0500

    update to use custom url for district

--- mobile/apis/getDistrict.ts
+++ mobile/apis/getDistrict.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const getDistrictApi = "https://k9spp0jupb.execute-api.us-east-1.amazonaws.com/prod/getDistrict"
+const getDistrictApi = "https://api-represent.gaskellsolutions.com/getDistrict"
 
 export function getAddressInfo(address: string) {
   return axios.get(`${getDistrictApi}?address=${address}`).then(res => res.data)
