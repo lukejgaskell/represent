c88446afe7f09967c5d4b119852ffe3fad9e3e16
Luke Gaskell <lukejgaskell@gmail.com>
2022-07-28 14:25:20 -0500

    fix url for getdistrict

--- mobile/apis/getDistrict.ts
+++ mobile/apis/getDistrict.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const getDistrictApi = "https://h9hwo88rtd.execute-api.us-east-1.amazonaws.com/prod/getDistrict"
+const getDistrictApi = "https://k9spp0jupb.execute-api.us-east-1.amazonaws.com/prod/getDistrict"
 
 export function getAddressInfo(address: string) {
   return axios.get(`${getDistrictApi}?address=${address}`).then(res => res.data)
